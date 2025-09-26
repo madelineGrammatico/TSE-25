520032286145ef4c259ab8982be296d721d23e87
Vi Dev <vi@videv.fr>
2025-09-26 19:38:58 +0200

    TSE-24 : ORDER > FILTERS > refacto > renommer variant à size

--- src/components/reusable-ui/Loader.tsx
+++ src/components/reusable-ui/Loader.tsx
@@ -4,19 +4,19 @@ import { ImSpinner8 } from "react-icons/im";
 import styled from "styled-components";
 
 type LoaderProps = {
-  variant?: FontSizeKey
+  size?: FontSizeKey
   color?: ColorValues
 }
 
-export const Loader = ({ variant = "SM", color = theme.colors.greyMedium }: LoaderProps) => {
+export const Loader = ({ size = "SM", color = theme.colors.greyMedium }: LoaderProps) => {
   return (
-    <LoaderStyled variant={variant ?? "SM"} color={color}>
+    <LoaderStyled size={size ?? "SM"} color={color}>
       <ImSpinner8 className="rotate-icon" />
     </LoaderStyled>)
 }
 
 type LoaderStyledProps = {
-  variant?: FontSizeKey
+  size?: FontSizeKey
   color?: ColorValues
 }
 
@@ -27,7 +27,7 @@ const LoaderStyled = styled.div<LoaderStyledProps>`
   height: 100%;
 
   .rotate-icon{
-    font-size: ${({ variant }) => theme.fonts.size[variant ?? "SM"]};
+    font-size: ${({ size }) => theme.fonts.size[size ?? "SM"]};
     animation: ${rotate} 1s linear infinite;
     color: ${({ color }) => color || theme.colors.primary};
   }
